414a659a0a0b4dadf0be30f7a8409f8b1b8a626f
mayank <mayank>
2020-07-20 16:23:32 +0530

    add navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 class App extends React.Component {
     render() {
-        return <div>Hello</div>;
+        return (
+            <div>
+                <Navbar />
+            </div>
+        );
     }
 }
 
